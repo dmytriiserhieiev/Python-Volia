f3c77dc3a0ff130ecb9b3c91b0d9fa1c1cde4e36
kamanyi-git <74266525+kamanyi-git@users.noreply.github.com>
2020-12-15 03:22:52 -0600

    conftest

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+# fixtures common for app and server
+import os
+
+import pytest
+
+
+BASE_DIR = os.path.dirname(__file__)
+
+
+@pytest.fixture
+def notebook_directory():
+    return os.path.join(BASE_DIR, 'notebooks')
+
+
+@pytest.fixture
+def print_notebook_url(base_url):
+    return base_url + "voila/render/print.ipynb"
+
+
+@pytest.fixture
+def voila_notebook(notebook_directory):
+    return os.path.join(notebook_directory, 'print.ipynb')
